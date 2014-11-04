35870698f6c9ce8d19a717ff16ec5a0b8d729c58
Vojta Biberle <vojtech.biberle@gmail.com>
2014-11-04 10:00:14 +0100

    Ruby1.9.3p550 + MySQL + Postgresql

---
+++ tmp/librarian/cache/source/chef/site/877777683730772c/yum-repoforge/version-uri/68bb4407fe741530/package/metadata.rb
@@ -0,0 +1,9 @@
+name 'yum-repoforge'
+maintainer 'Chef'
+maintainer_email 'Sean OMeara <someara@getchef.com>'
+license 'Apache 2.0'
+description 'Installs/Configures yum-repoforge'
+version '0.4.0'
+
+depends 'yum', '~> 3.0'
+depends 'yum-epel'
